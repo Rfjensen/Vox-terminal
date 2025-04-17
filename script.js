33b917c452e63073807b7cac007147be55c80622
Ross <rossjensen88@gmail.com>
2025-04-16 23:33:45 -0500

    Revert "Update script.js"
    This reverts commit a403172560889f889b17f7667524f9453fd7f946.

--- script.js
+++ script.js
@@ -160,7 +160,7 @@ function runBootSequence() {
   let count = 0;
   const bootInterval = setInterval(() => {
     if (count < bootMessages.length) {
-      const messageElement = document.createElement('div');
+      onst messageElement = document.createElement('div');
       messageElement.textContent = bootMessages[count];
       output.appendChild(messageElement);
       
